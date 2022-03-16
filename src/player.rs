b806c6c669fed2f7ad978216937892c895fafea3
irayspace <irayspacii@gmail.com>
2022-03-16 23:45:04 +0800

    feat: added player state machine

--- src/player.rs
+++ src/player.rs
@@ -3,9 +3,16 @@ use crate::loading::SpritesheetAssets;
 use crate::AppState;
 use bevy::prelude::*;
 
+#[derive(Debug, PartialEq)]
+pub enum PlayerStateMachine {
+    Idle,
+    Moving,
+}
+
 #[derive(Component, Debug)]
 pub struct Player {
     pub speed: f32,
+    pub state: PlayerStateMachine,
 }
 
 fn render_player(
@@ -43,25 +50,49 @@ fn render_player(
                 AnimationValue {
                     frames: vec![12, 13, 14, 15],
                 },
+                AnimationValue { frames: vec![0] },
             ],
             current_value: 0,
             current_frame: 0,
             duration: Timer::from_seconds(0.1, true),
         })
-        .insert(Player { speed: 200. });
+        .insert(Player {
+            speed: 200.,
+            state: PlayerStateMachine::Idle,
+        });
 }
 
-// TODO: стейт-машины анимации
-fn read_input(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform)>, time: Res<Time>) {
-    if let Ok((player, mut transform)) = query.get_single_mut() {
+fn read_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Player, &mut Transform)>,
+    time: Res<Time>,
+) {
+    if let Ok((mut player, mut transform)) = query.get_single_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
+            player.state = PlayerStateMachine::Moving;
             transform.translation.y += player.speed * time.delta_seconds();
         } else if keyboard_input.pressed(KeyCode::Down) {
+            player.state = PlayerStateMachine::Moving;
             transform.translation.y -= player.speed * time.delta_seconds();
         } else if keyboard_input.pressed(KeyCode::Left) {
+            player.state = PlayerStateMachine::Moving;
             transform.translation.x -= player.speed * time.delta_seconds();
         } else if keyboard_input.pressed(KeyCode::Right) {
+            player.state = PlayerStateMachine::Moving;
             transform.translation.x += player.speed * time.delta_seconds();
+        } else {
+            player.state = PlayerStateMachine::Idle;
+        }
+    }
+}
+
+fn update_state(mut query: Query<(&Player, &mut Animation)>) {
+    if let Ok((player, mut animation)) = query.get_single_mut() {
+        if player.state == PlayerStateMachine::Idle {
+            animation.current_value = 4;
+            animation.current_frame = 0;
+        } else if player.state == PlayerStateMachine::Moving {
+            animation.current_value = 0;
         }
     }
 }
@@ -71,6 +102,10 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(AppState::InGame).with_system(render_player))
-            .add_system_set(SystemSet::on_update(AppState::InGame).with_system(read_input));
+            .add_system_set(
+                SystemSet::on_update(AppState::InGame)
+                    .with_system(read_input)
+                    .with_system(update_state),
+            );
     }
 }
