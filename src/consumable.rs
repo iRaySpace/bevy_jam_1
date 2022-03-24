e747e98c883902c23ae9014e0b2fce96f3c87990
irayspace <irayspacii@gmail.com>
2022-03-24 17:24:31 +0800

    feat: remove consumable initial

--- src/consumable.rs
+++ src/consumable.rs
@@ -14,43 +14,6 @@ pub enum Consumable {
 
 struct ConsumableSpawnTimer(Timer);
 
-fn render_consumable(
-    mut commands: Commands,
-    spritesheet_assets: Res<SpritesheetAssets>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let size = Vec2::new(16., 16.);
-
-    let texture = spritesheet_assets.milk_grass.clone();
-    let texture_atlas = TextureAtlas::from_grid(texture, size, 4, 1);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    commands
-        .spawn_bundle(SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(3.0)).with_translation(Vec3::new(
-                64.,
-                64.,
-                crate::z::CONSUMABLES,
-            )),
-            sprite: TextureAtlasSprite {
-                index: Consumable::Grass as usize,
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .insert(RigidBody::Dynamic)
-        .insert(CollisionShape::Cuboid {
-            half_extends: size.extend(0.0) / 2.0,
-            border_radius: None,
-        })
-        .insert(
-            CollisionLayers::none()
-                .with_group(crate::physics::Layer::Consumable)
-                .with_mask(crate::physics::Layer::Player),
-        );
-}
-
 fn spawn_consumable(
     mut timer: ResMut<ConsumableSpawnTimer>,
     time: Res<Time>,
@@ -100,7 +63,6 @@ pub struct ConsumablePlugin;
 impl Plugin for ConsumablePlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(ConsumableSpawnTimer(Timer::from_seconds(2.0, true)))
-            .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(render_consumable))
             .add_system_set(SystemSet::on_update(AppState::InGame).with_system(spawn_consumable));
     }
 }
